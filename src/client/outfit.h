77507d78ddd69b31460cd3818b3109d4cbf09fc4
G.Joker👻 <125809602+Mateuzkl@users.noreply.github.com>
2026-02-17 17:42:55 -0300

    client: add creature and object shadows

--- src/client/outfit.h
+++ src/client/outfit.h
@@ -20,101 +20,105 @@
  * THE SOFTWARE.
  */
 
- #ifndef OUTFIT_H
- #define OUTFIT_H
- 
- #include <framework/util/color.h>
- #include "thingtypemanager.h"
- #include <framework/graphics/drawqueue.h>
- 
- class Outfit
- {
- protected:
-     Point wingsOffset;
- public:
-     Outfit();
- 
-     static Color getColor(int color)
-     {
-         return Color::getOutfitColor(color);
-     }
- 
-     void draw(Point dest, Otc::Direction direction, uint walkAnimationPhase, bool animate = true, LightView* lightView = nullptr, bool ui = false);
-     void draw(const Rect& dest, Otc::Direction direction, uint animationPhase, bool animate = true, bool ui = false, bool oldScaling = false);
- 
-     void setId(int id) { m_id = id; }
-     void setAuxId(int id) { m_auxId = id; }
-     void setHead(int head) { m_head = head; }
-     void setBody(int body) { m_body = body; }
-     void setLegs(int legs) { m_legs = legs; }
-     void setFeet(int feet) { m_feet = feet; }
-     void setAddons(int addons) { m_addons = addons; }
-     void setMount(int mount) { m_mount = mount; }
-     void setWings(int wings) { m_wings = wings; }
-     void setAura(int aura) { m_aura = aura; }
-     void setCategory(ThingCategory category) { m_category = category; }
-     void setShader(const std::string& shader) { m_shader = shader; }
-     void setHealthBar(uint8 id) { m_healthBar = id; }
-     void setManaBar(uint8 id) { m_manaBar = id; }
-     void setCenter(bool value) { m_center = value; }
- 
-     void resetClothes();
-     void resetShader() { m_shader = ""; }
-     void setWingsOffset(Point offset) { wingsOffset = offset; }
-     int getId() const { return m_id; }
-     int getAuxId() const { return m_auxId; }
-     int getHead() const { return m_head; }
-     int getBody() const { return m_body; }
-     int getLegs() const { return m_legs; }
-     int getFeet() const { return m_feet; }
-     int getAddons() const { return m_addons; }
-     int getMount() const { return m_mount; }
-     int getWings() const { return m_wings; }
-     int getAura() const { return m_aura; }
-     ThingCategory getCategory() const { return m_category; }
-     std::string getShader() const { return m_shader; }
-     int getHealthBar() const { return m_healthBar; }
-     int getManaBar() const { return m_manaBar; }
- 
- private:
-     ThingCategory m_category;
-     int m_id, m_auxId, m_head, m_body, m_legs, m_feet, m_addons, m_mount = 0, m_wings = 0, m_aura = 0;
-     int m_healthBar = 0, m_manaBar = 0;
-     std::string m_shader;
-     bool m_center = false;
- };
- 
- struct DrawQueueItemOutfit : public DrawQueueItemTexturedRect {
-     DrawQueueItemOutfit(const Rect& rect, const TexturePtr& texture, const Rect& src, const Point& offset, int32_t colors, const Color& color) :
-         DrawQueueItemTexturedRect(rect, texture, src, color), m_offset(offset), m_colors(colors)
-     {};
- 
-     void draw() override;
-     void draw(const Point& pos) override;
-     bool cache() override;
- 
-     Point m_offset;
-     int32_t m_colors;
- };
- 
- struct DrawQueueItemOutfitWithShader : public DrawQueueItemTexturedRect {
-     DrawQueueItemOutfitWithShader(const Rect& rect, const TexturePtr& texture, const Rect& src, const Point& offset, const Point& center, int32_t colors, const std::string& shader) :
-         DrawQueueItemTexturedRect(rect, texture, src, Color::white), m_offset(offset), m_center(center), m_colors(colors), m_shader(shader)
-     {};
- 
-     void draw() override;
-     void draw(const Point& pos) override
-     {}
-     bool cache() override
-     {
-         return false;
-     }
- 
-     Point m_offset;
-     Point m_center;
-     int32_t m_colors;
-     std::string m_shader;
- };
- 
- #endif
- 
+#ifndef OUTFIT_H
+#define OUTFIT_H
+
+#include "thingtypemanager.h"
+#include <framework/graphics/drawqueue.h>
+#include <framework/util/color.h>
+
+class Outfit {
+protected:
+  Point wingsOffset;
+
+public:
+  Outfit();
+
+  static Color getColor(int color) { return Color::getOutfitColor(color); }
+
+  void draw(Point dest, Otc::Direction direction, uint walkAnimationPhase,
+            bool animate = true, LightView *lightView = nullptr,
+            bool ui = false);
+  void draw(const Rect &dest, Otc::Direction direction, uint animationPhase,
+            bool animate = true, bool ui = false, bool oldScaling = false);
+  void drawShadow(Point dest, Otc::Direction direction, uint walkAnimationPhase,
+                  bool animate = true);
+
+  void setId(int id) { m_id = id; }
+  void setAuxId(int id) { m_auxId = id; }
+  void setHead(int head) { m_head = head; }
+  void setBody(int body) { m_body = body; }
+  void setLegs(int legs) { m_legs = legs; }
+  void setFeet(int feet) { m_feet = feet; }
+  void setAddons(int addons) { m_addons = addons; }
+  void setMount(int mount) { m_mount = mount; }
+  void setWings(int wings) { m_wings = wings; }
+  void setAura(int aura) { m_aura = aura; }
+  void setCategory(ThingCategory category) { m_category = category; }
+  void setShader(const std::string &shader) { m_shader = shader; }
+  void setHealthBar(uint8 id) { m_healthBar = id; }
+  void setManaBar(uint8 id) { m_manaBar = id; }
+  void setCenter(bool value) { m_center = value; }
+
+  void resetClothes();
+  void resetShader() { m_shader = ""; }
+  void setWingsOffset(Point offset) { wingsOffset = offset; }
+  int getId() const { return m_id; }
+  int getAuxId() const { return m_auxId; }
+  int getHead() const { return m_head; }
+  int getBody() const { return m_body; }
+  int getLegs() const { return m_legs; }
+  int getFeet() const { return m_feet; }
+  int getAddons() const { return m_addons; }
+  int getMount() const { return m_mount; }
+  int getWings() const { return m_wings; }
+  int getAura() const { return m_aura; }
+  ThingCategory getCategory() const { return m_category; }
+  std::string getShader() const { return m_shader; }
+  int getHealthBar() const { return m_healthBar; }
+  int getManaBar() const { return m_manaBar; }
+
+private:
+  ThingCategory m_category;
+  int m_id, m_auxId, m_head, m_body, m_legs, m_feet, m_addons,
+      m_mount = 0, m_wings = 0, m_aura = 0;
+  int m_healthBar = 0, m_manaBar = 0;
+  std::string m_shader;
+  bool m_center = false;
+};
+
+struct DrawQueueItemOutfit : public DrawQueueItemTexturedRect {
+  DrawQueueItemOutfit(const Rect &rect, const TexturePtr &texture,
+                      const Rect &src, const Point &offset, int32_t colors,
+                      const Color &color)
+      : DrawQueueItemTexturedRect(rect, texture, src, color), m_offset(offset),
+        m_colors(colors) {};
+
+  void draw() override;
+  void draw(const Point &pos) override;
+  bool cache() override;
+
+  Point m_offset;
+  int32_t m_colors;
+};
+
+struct DrawQueueItemOutfitWithShader : public DrawQueueItemTexturedRect {
+  DrawQueueItemOutfitWithShader(const Rect &rect, const TexturePtr &texture,
+                                const Rect &src, const Point &offset,
+                                const Point &center, int32_t colors,
+                                const std::string &shader)
+      : DrawQueueItemTexturedRect(rect, texture, src, Color::white),
+        m_offset(offset), m_center(center), m_colors(colors),
+        m_shader(shader) {};
+
+  void draw() override;
+  void draw(const Point &pos) override {}
+  bool cache() override { return false; }
+
+  Point m_offset;
+  Point m_center;
+  int32_t m_colors;
+  std::string m_shader;
+};
+
+#endif
--- src/client/tile.h
+++ src/client/tile.h
@@ -23,175 +23,176 @@
 #ifndef TILE_H
 #define TILE_H
 
-#include "declarations.h"
-#include "mapview.h"
-#include "effect.h"
 #include "creature.h"
+#include "declarations.h"
+#include "effect.h"
 #include "item.h"
+#include "mapview.h"
 #include <framework/luaengine/luaobject.h>
 #include <framework/stdext/time.h>
 
-enum tileflags_t
-{
-    TILESTATE_NONE = 0,
-    TILESTATE_PROTECTIONZONE = 1 << 0,
-    TILESTATE_TRASHED = 1 << 1,
-    TILESTATE_OPTIONALZONE = 1 << 2,
-    TILESTATE_NOLOGOUT = 1 << 3,
-    TILESTATE_HARDCOREZONE = 1 << 4,
-    TILESTATE_REFRESH = 1 << 5,
+enum tileflags_t {
+  TILESTATE_NONE = 0,
+  TILESTATE_PROTECTIONZONE = 1 << 0,
+  TILESTATE_TRASHED = 1 << 1,
+  TILESTATE_OPTIONALZONE = 1 << 2,
+  TILESTATE_NOLOGOUT = 1 << 3,
+  TILESTATE_HARDCOREZONE = 1 << 4,
+  TILESTATE_REFRESH = 1 << 5,
 
-    // internal usage
-    TILESTATE_HOUSE = 1 << 6,
-    TILESTATE_TELEPORT = 1 << 17,
-    TILESTATE_MAGICFIELD = 1 << 18,
-    TILESTATE_MAILBOX = 1 << 19,
-    TILESTATE_TRASHHOLDER = 1 << 20,
-    TILESTATE_BED = 1 << 21,
-    TILESTATE_DEPOT = 1 << 22,
-    TILESTATE_TRANSLUECENT_LIGHT = 1 << 23,
+  // internal usage
+  TILESTATE_HOUSE = 1 << 6,
+  TILESTATE_TELEPORT = 1 << 17,
+  TILESTATE_MAGICFIELD = 1 << 18,
+  TILESTATE_MAILBOX = 1 << 19,
+  TILESTATE_TRASHHOLDER = 1 << 20,
+  TILESTATE_BED = 1 << 21,
+  TILESTATE_DEPOT = 1 << 22,
+  TILESTATE_TRANSLUECENT_LIGHT = 1 << 23,
 
-    TILESTATE_LAST = 1 << 24
+  TILESTATE_LAST = 1 << 24
 };
 
-class Tile : public LuaObject
-{
+class Tile : public LuaObject {
 public:
-    enum {
-        MAX_THINGS = 10
-    };
+  enum { MAX_THINGS = 10 };
 
-    Tile(const Position& position);
+  Tile(const Position &position);
 
-    void calculateCorpseCorrection();
+  void calculateCorpseCorrection();
 
-    void drawGround(const Point& dest, LightView* lightView = nullptr);
-    void drawBottom(const Point& dest, LightView* lightView = nullptr);
-    void drawCreatures(const Point& dest, LightView* lightView = nullptr);
-    void drawTop(const Point& dest, LightView* lightView = nullptr);
-    void drawTexts(Point dest);
-    void drawWidget(Point dest);
+  void drawGround(const Point &dest, LightView *lightView = nullptr);
+  void drawBottomShadows(const Point &dest);
+  void drawBottom(const Point &dest, LightView *lightView = nullptr);
+  void drawCreatureShadows(const Point &dest);
+  void drawCreatures(const Point &dest, LightView *lightView = nullptr);
+  void drawTop(const Point &dest, LightView *lightView = nullptr);
+  void drawTexts(Point dest);
+  void drawWidget(Point dest);
 
 public:
-    void clean();
+  void clean();
 
-    void addWalkingCreature(const CreaturePtr& creature);
-    void removeWalkingCreature(const CreaturePtr& creature);
+  void addWalkingCreature(const CreaturePtr &creature);
+  void removeWalkingCreature(const CreaturePtr &creature);
 
-    void addThing(const ThingPtr& thing, int stackPos);
-    bool removeThing(ThingPtr thing);
-    ThingPtr getThing(int stackPos);
-    EffectPtr getEffect(uint16 id);
-    bool hasThing(const ThingPtr& thing);
-    int getThingStackPos(const ThingPtr& thing);
-    ThingPtr getTopThing();
+  void addThing(const ThingPtr &thing, int stackPos);
+  bool removeThing(ThingPtr thing);
+  ThingPtr getThing(int stackPos);
+  EffectPtr getEffect(uint16 id);
+  bool hasThing(const ThingPtr &thing);
+  int getThingStackPos(const ThingPtr &thing);
+  ThingPtr getTopThing();
 
-    ThingPtr getTopLookThing();
-    ThingPtr getTopLookThingEx(Point offset);
-    ThingPtr getTopUseThing();
-    CreaturePtr getTopCreature();
-    CreaturePtr getTopCreatureEx(Point offset);
-    ThingPtr getTopMoveThing();
-    ThingPtr getTopMultiUseThing();
-    ThingPtr getTopMultiUseThingEx(Point offset);
+  ThingPtr getTopLookThing();
+  ThingPtr getTopLookThingEx(Point offset);
+  ThingPtr getTopUseThing();
+  CreaturePtr getTopCreature();
+  CreaturePtr getTopCreatureEx(Point offset);
+  ThingPtr getTopMoveThing();
+  ThingPtr getTopMultiUseThing();
+  ThingPtr getTopMultiUseThingEx(Point offset);
 
-    const Position& getPosition() { return m_position; }
-    int getDrawElevation() { return m_drawElevation; }
-    std::vector<ItemPtr> getItems();
-    std::vector<CreaturePtr> getCreatures();
-    std::vector<CreaturePtr> getWalkingCreatures() { return m_walkingCreatures; }
-    std::vector<ThingPtr> getThings() { return m_things; }
-    std::vector<EffectPtr> getEffects() { return m_effects; }
-    ItemPtr getGround();
-    int getGroundSpeed();
-    bool isBlocking() { return m_blocking != 0; }
-    uint8 getMinimapColorByte();
-    int getThingCount() { return m_things.size() + m_effects.size(); }
-    bool isPathable();
-    bool isWalkable(bool ignoreCreatures = false);
-    bool isFullGround();
-    bool isFullyOpaque();
-    bool isSingleDimension();
-    bool isLookPossible();
-    bool isBlockingProjectile();
-    bool isClickable();
-    bool isEmpty();
-    bool isDrawable();
-    bool hasTranslucentLight() { return m_flags & TILESTATE_TRANSLUECENT_LIGHT; }
-    bool mustHookSouth();
-    bool mustHookEast();
-    bool hasCreature();
-    bool hasBlockingCreature();
-    bool limitsFloorsView(bool isFreeView = false);
-    bool canErase();
-    int getElevation();
-    bool hasElevation(int elevation = 1);
-    void overwriteMinimapColor(uint8 color) { m_minimapColor = color; }
+  const Position &getPosition() { return m_position; }
+  int getDrawElevation() { return m_drawElevation; }
+  std::vector<ItemPtr> getItems();
+  std::vector<CreaturePtr> getCreatures();
+  std::vector<CreaturePtr> getWalkingCreatures() { return m_walkingCreatures; }
+  std::vector<ThingPtr> getThings() { return m_things; }
+  std::vector<EffectPtr> getEffects() { return m_effects; }
+  ItemPtr getGround();
+  int getGroundSpeed();
+  bool isBlocking() { return m_blocking != 0; }
+  uint8 getMinimapColorByte();
+  int getThingCount() { return m_things.size() + m_effects.size(); }
+  bool isPathable();
+  bool isWalkable(bool ignoreCreatures = false);
+  bool isFullGround();
+  bool isFullyOpaque();
+  bool isSingleDimension();
+  bool isLookPossible();
+  bool isBlockingProjectile();
+  bool isClickable();
+  bool isEmpty();
+  bool isDrawable();
+  bool hasTranslucentLight() { return m_flags & TILESTATE_TRANSLUECENT_LIGHT; }
+  bool mustHookSouth();
+  bool mustHookEast();
+  bool hasCreature();
+  bool hasBlockingCreature();
+  bool limitsFloorsView(bool isFreeView = false);
+  bool canErase();
+  int getElevation();
+  bool hasElevation(int elevation = 1);
+  void overwriteMinimapColor(uint8 color) { m_minimapColor = color; }
 
-    void remFlag(uint32 flag) { m_flags &= ~flag; }
-    void setFlag(uint32 flag) { m_flags |= flag; }
-    void setFlags(uint32 flags) { m_flags = flags; }
-    bool hasFlag(uint32 flag) { return (m_flags & flag) == flag; }
-    uint32 getFlags() { return m_flags; }
+  void remFlag(uint32 flag) { m_flags &= ~flag; }
+  void setFlag(uint32 flag) { m_flags |= flag; }
+  void setFlags(uint32 flags) { m_flags = flags; }
+  bool hasFlag(uint32 flag) { return (m_flags & flag) == flag; }
+  uint32 getFlags() { return m_flags; }
 
-    void setHouseId(uint32 hid) { m_houseId = hid; }
-    uint32 getHouseId() { return m_houseId; }
-    bool isHouseTile() { return m_houseId != 0 && (m_flags & TILESTATE_HOUSE) == TILESTATE_HOUSE; }
+  void setHouseId(uint32 hid) { m_houseId = hid; }
+  uint32 getHouseId() { return m_houseId; }
+  bool isHouseTile() {
+    return m_houseId != 0 && (m_flags & TILESTATE_HOUSE) == TILESTATE_HOUSE;
+  }
 
-    void select() { m_selected = true; }
-    void unselect() { m_selected = false; }
-    bool isSelected() { return m_selected; }
+  void select() { m_selected = true; }
+  void unselect() { m_selected = false; }
+  bool isSelected() { return m_selected; }
 
-    TilePtr asTile() { return static_self_cast<Tile>(); }
+  TilePtr asTile() { return static_self_cast<Tile>(); }
 
-    void setSpeed(uint16_t speed, uint8_t blocking) {
-        m_speed = speed;
-        m_blocking = blocking;
+  void setSpeed(uint16_t speed, uint8_t blocking) {
+    m_speed = speed;
+    m_blocking = blocking;
+  }
+
+  void setText(const std::string &text, Color color);
+  std::string getText();
+  void setTimer(int time, Color color);
+  int getTimer();
+  void setFill(Color color);
+  void resetFill() { m_fill = Color::alpha; }
+
+  bool canShoot(int distance);
+
+  void setWidget(UIWidgetPtr widget) { m_widget = widget; }
+  UIWidgetPtr getWidget() { return m_widget; }
+  void removeWidget() {
+    if (m_widget) {
+      m_widget->destroy();
+      m_widget = nullptr;
     }
-
-    void setText(const std::string& text, Color color);
-    std::string getText();
-    void setTimer(int time, Color color);
-    int getTimer();
-    void setFill(Color color);
-    void resetFill() { m_fill = Color::alpha; }
-
-    bool canShoot(int distance);
-	
-    void setWidget(UIWidgetPtr widget) { m_widget = widget; }
-    UIWidgetPtr getWidget() { return m_widget; }
-    void removeWidget() {
-        if (m_widget) {
-            m_widget->destroy();
-            m_widget = nullptr;
-        }
-    }
+  }
 
 private:
-    void checkTranslucentLight();
+  void checkTranslucentLight();
 
-    std::vector<CreaturePtr> m_walkingCreatures;
-    std::vector<EffectPtr> m_effects; // leave this outside m_things because it has no stackpos.
-    std::vector<ThingPtr> m_things;
-    Position m_position;
-    uint8 m_drawElevation;
-    uint8 m_minimapColor;
-    uint32 m_flags, m_houseId;
-    uint16 m_speed = 0;
-    uint8 m_blocking = 0;
+  std::vector<CreaturePtr> m_walkingCreatures;
+  std::vector<EffectPtr>
+      m_effects; // leave this outside m_things because it has no stackpos.
+  std::vector<ThingPtr> m_things;
+  Position m_position;
+  uint8 m_drawElevation;
+  uint8 m_minimapColor;
+  uint32 m_flags, m_houseId;
+  uint16 m_speed = 0;
+  uint8 m_blocking = 0;
 
-    uint32_t m_lastCreature = 0;
-    int m_topCorrection = 0;
-    int m_topDraws = 0;
-    
-    stdext::boolean<false> m_selected;
+  uint32_t m_lastCreature = 0;
+  int m_topCorrection = 0;
+  int m_topDraws = 0;
 
-    ticks_t m_timer = 0;
-    StaticTextPtr m_timerText;
-    StaticTextPtr m_text;
-    Color m_fill = Color::alpha;
-	
-	UIWidgetPtr m_widget;
+  stdext::boolean<false> m_selected;
+
+  ticks_t m_timer = 0;
+  StaticTextPtr m_timerText;
+  StaticTextPtr m_text;
+  Color m_fill = Color::alpha;
+
+  UIWidgetPtr m_widget;
 };
 
 #endif
